78d35429b0b643d6af8e70ac2d9d24fa78800f19
Vlad Ivan <->
2020-05-18 08:15:40 +0300

    1

---
+++ PracticalTest02/app/src/main/java/eim/practitcaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,14 @@
+package eim.practitcaltest02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+    }
+}
